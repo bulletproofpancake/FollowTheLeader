e0e70b0b7d7f8f463e06ffe727a7c74c9c02cda2
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-07-23 23:22:19 +0800

    holding space gives a "brake" effect on the followers

--- Assets/Scripts/Follower.cs
+++ Assets/Scripts/Follower.cs
@@ -21,6 +21,7 @@ public class Follower : MonoBehaviour
 
     private void Move()
     {
+        speed = Input.GetKey(KeyCode.Space) ? speed > 0 ? speed -= Time.deltaTime * 5 : 0 : 2.75f;
         _distanceTravelled += speed * Time.deltaTime;
         transform.position = _pathCreator.path.GetPointAtDistance(_distanceTravelled);
         transform.rotation = _pathCreator.path.GetRotationAtDistance(_distanceTravelled);
