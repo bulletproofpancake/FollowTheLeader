5d6e2596f5dd2fbe03ef775d0ab0ded481104dc0
Jolo Gutierrez <57520402+bulletproofpancake@users.noreply.github.com>
2021-07-24 20:50:46 +0800

    removed unnecessary stuff

--- Assets/Scripts/LapCounter.cs
+++ Assets/Scripts/LapCounter.cs
@@ -3,32 +3,13 @@ using UnityEngine;
 
 public class LapCounter : MonoBehaviour
 {
-    public static event Action ScoreLap;
-
     public static event Action SpawnFollower;
 
-    private bool _hasGameStarted;
-
-    private void OnEnable()
-    {
-        GameManager.GameStart += OnGameStart;
-    }
-    private void OnDisable()
-    {
-        GameManager.GameStart -= OnGameStart;
-    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (!other.CompareTag("Player")) return;
         SpawnFollower?.Invoke();
-        if (!_hasGameStarted) return;
-        ScoreLap?.Invoke();
-    }
-
-    private void OnGameStart()
-    {
-        _hasGameStarted = true;
     }
     
 }
--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,7 +5,7 @@ public class UIManager : MonoBehaviour
 {
     [SerializeField] private GameObject titleScreen;
     [SerializeField] private GameObject endScreen;
-    [SerializeField] private TextMeshProUGUI lapsDisplay;
+    //[SerializeField] private TextMeshProUGUI lapsDisplay;
     [SerializeField] private TextMeshProUGUI employeeDisplay;
 
     private void ShowEndScreen()
@@ -15,12 +15,12 @@ public class UIManager : MonoBehaviour
 
     private void Start()
     {
-        lapsDisplay.text = $"Laps: {ScoreManager.Instance.LapCount}/{ScoreManager.Instance.MaxLaps}";
+        //lapsDisplay.text = $"Laps: {ScoreManager.Instance.LapCount}/{ScoreManager.Instance.MaxLaps}";
     }
     
     private void Update()
     {
-        lapsDisplay.text = $"Laps: {ScoreManager.Instance.LapCount}/{ScoreManager.Instance.MaxLaps}";
+        //lapsDisplay.text = $"Laps: {ScoreManager.Instance.LapCount}/{ScoreManager.Instance.MaxLaps}";
         employeeDisplay.text = $"Employees: {PlayerFollower.Instance.followers.Count:00}";
         titleScreen.SetActive(!GameManager.Instance.hasGameStarted && !GameManager.Instance.isGameOver);
         endScreen.SetActive(GameManager.Instance.isGameOver);
